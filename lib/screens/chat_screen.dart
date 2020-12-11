bf913367bfde3d9a95d2e605d662acd91d6d9cc7
DeleMike <akindelemichael65@gmail.com>
2020-12-11 22:19:09 +0100

    add logout functionality

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class ChatScreen extends StatelessWidget {
   @override
@@ -7,14 +8,43 @@ class ChatScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Chat App'),
+        actions: [
+          DropdownButton(
+            icon: Icon(
+              Icons.more_vert,
+              color: Theme.of(context).primaryIconTheme.color,
+            ),
+            items: [
+              DropdownMenuItem(
+                child: Container(
+                  child: Row(
+                    children: [
+                      Icon(Icons.exit_to_app),
+                      SizedBox(width: 8),
+                      Text('Logout'),
+                    ],
+                  ),
+                ),
+                value: 'logout'
+              )
+            ],
+            onChanged: (itemIdentifier){
+              if(itemIdentifier == 'logout'){
+                FirebaseAuth.instance.signOut();
+              }
+            },
+          )
+        ],
       ),
       body: StreamBuilder(
         stream: FirebaseFirestore.instance
             .collection('chats/Ex46OnjPWwZZg7dloDxa/messages')
             .snapshots(),
         builder: (ctx, streamSnapshot) {
-          if(streamSnapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator(),);
+          if (streamSnapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
           }
           final documents = streamSnapshot.data.docs;
           return ListView.builder(
@@ -29,9 +59,9 @@ class ChatScreen extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () {
-          FirebaseFirestore.instance.collection('chats/Ex46OnjPWwZZg7dloDxa/messages').add({
-            'text': 'This was added by clicking FAB button'
-          });
+          FirebaseFirestore.instance
+              .collection('chats/Ex46OnjPWwZZg7dloDxa/messages')
+              .add({'text': 'This was added by clicking FAB button'});
         },
       ),
     );
